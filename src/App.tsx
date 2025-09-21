2ed7efecad477956f223f76000249c9058dd3a95
aarondelen <aarondelen.ad@gmail.com>
2025-09-22 03:49:19 +0800

    Almost done in Hero, will be doing final touches with the mouse movement parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import ButtonGradient from "./assets/svg/ButtonGradient"
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <>
-    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-      <Navbar />
-    </div>
-    <ButtonGradient/>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Navbar />
+        <Hero />
+      </div>
+      <ButtonGradient />
     </>
   )
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -17,7 +17,7 @@ const Button = ({
   px,
   white,
 }: ButtonProps) => {
-  const classes = `button cursor-pointer relative items-center justify-center h-11 transition-colors hover:text-color-1 ${ px || "px-7"} ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+  const classes = `button cursor-pointer relative items-center justify-center h-11 transition-colors hover:text-color-1 ${px || "px-7"} ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
   const spanClasses = "relative z-10";
 
   const renderButton = () => (
