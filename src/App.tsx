e6efd78949cd1b08eff67069e86610d121dce895
aarondelen <aarondelen.ad@gmail.com>
2025-09-22 12:23:59 +0800

    Done with benefits section, stupid clipPath, what a struggle man, misplaced the style

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient"
+import Benefits from "./components/Benefits"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
@@ -9,6 +10,7 @@ function App() {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Navbar />
         <Hero />
+        <Benefits/>
       </div>
       <ButtonGradient />
     </>
